2d2be6c75d69c53075726adff1971670189b99d0
nag-jun <junichi2002.n@gmail.com>
2023-05-03 14:45:33 +0900

    edit UI etc

---
+++ Assets/Script/RaycastMask.cs
@@ -0,0 +1,51 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Image))]
+public class RaycastMask : MonoBehaviour
+{
+    [SerializeField, HideInInspector]
+    private Image _image;
+
+    [SerializeField, Range(0f, 1f)]
+    private float _threshold = 0f;
+
+#if UNITY_EDITOR
+
+    private void Reset()
+    {
+        _image = GetComponent<Image>();
+        _image.alphaHitTestMinimumThreshold = _threshold;
+    }
+    private void OnValidate()
+    {
+        _image.alphaHitTestMinimumThreshold = _threshold;
+
+        if (_image.sprite == null)
+            return;
+
+        if (_threshold <= 0f)
+            return;
+
+        // GetPixel ‚ð‚·‚é‚½‚ß‚ÉReadWrite‚ª—LŒø‚Å‚ ‚é•K—v‚ª‚ ‚é
+        var path = UnityEditor.AssetDatabase.GetAssetPath(_image.sprite);
+        var importer = UnityEditor.AssetImporter.GetAtPath(path) as UnityEditor.TextureImporter;
+        if (!importer.isReadable)
+        {
+            importer.isReadable = true;
+            UnityEditor.EditorUtility.SetDirty(_image.sprite);
+            UnityEditor.AssetDatabase.SaveAssets();
+        }
+
+        var settings = new UnityEditor.TextureImporterSettings();
+        importer.ReadTextureSettings(settings);
+        if (settings.spriteMeshType != SpriteMeshType.FullRect)
+        {
+            settings.spriteMeshType = SpriteMeshType.FullRect;
+            importer.SetTextureSettings(settings);
+            importer.SaveAndReimport();
+        }
+    }
+
+#endif
+}
--- Assets/Script/ScoreText.cs
+++ Assets/Script/ScoreText.cs
@@ -5,20 +5,12 @@ using TMPro;
 
 public class ScoreText : MonoBehaviour
 {
-    // 1問正解 +10点　// 1問不正解 +9点(10 * 0.9)
-
-    // スコア計算 = 正解数 × 10 × 不正解数 × 9 × 正解数ボーナス(正解数 × 1.1)
-
-
     public TextMeshProUGUI scoreText;
     int totalScore;
-
-
 
     void Start()
     {
         totalScore = QuizManager.GetTotalScore();
-
 
         scoreText.text = string.Format("Score /{0}" , totalScore);
 
