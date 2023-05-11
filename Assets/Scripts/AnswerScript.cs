47cd9dc0983799505dbbff3d21f73062beed7820
nag-jun <junichi2002.n@gmail.com>
2023-05-11 12:53:12 +0900

    edit C&W

---
+++ Assets/Scripts/AnswerScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+    [SerializeField] GameObject maru, batsu;
+
+
+    //private void Start()
+   // {
+    //    maru.SetActive(false);
+    //    batsu.SetActive(false);
+    //}
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            maru.SetActive(true);
+            //Debug.Log("Yes");
+            quizManager.Correct();
+            StartCoroutine("SetText");
+        }
+        else 
+        {
+            batsu.SetActive(true);
+            //Debug.Log("No");
+            quizManager.Wrong();
+            StartCoroutine("SetText2");
+        }
+    }
+
+    //Correct
+    IEnumerator SetText()
+    {
+        yield return new WaitForSeconds(0.3f);
+        maru.SetActive(false);
+    }
+
+    //Wrong
+    IEnumerator SetText2()
+    {
+        yield return new WaitForSeconds(0.3f);
+        batsu.SetActive(false);
+    }
+}
--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class QuizManager : MonoBehaviour
 {
     public List<QuestionandAnswers> QandA;
     public GameObject[] options;
+
     //質問の数
     public int currentQuestion;
 
-    public TextMeshProUGUI Qtxt,scoreTxt;
+    public TextMeshProUGUI Qtxt;
+
+
 
     public int correctCount = 0;
     public int wrongCount = 0;
@@ -27,25 +31,29 @@ public class QuizManager : MonoBehaviour
     public void Correct()
     {
         correctCount += 100;
+
         QandA.RemoveAt(currentQuestion);
         GneraleQuestion();
     }
 
     public void Wrong()
     {
-        wrongCount += 80;
-        
+        wrongCount -= 20;
+        //×パネルの表示
+        //batsuFlug = true;
+
+        //int qWrong = QandA.qrowget(); //CSVファイルの行数を取得
+        //PlayerPrefs.SetInt("missrow" + qrow.ToString(), qWrong); //行数をキーにして間違えた行数を格納
+        //PlayerPrefs.Save(); //保存
+
         QandA.RemoveAt(currentQuestion);
         GneraleQuestion();
     }
 
-
-  
-
     void SetAnswers()
     {
         for (int i = 0; i < options.Length; i++)
-        {
+        { 
 
             //テキスト一つ一つに割り当てる。
             options[i].transform.GetChild(0).GetComponent<TextMeshProUGUI>().text = QandA[currentQuestion].Answers[i];
@@ -54,6 +62,7 @@ public class QuizManager : MonoBehaviour
             if (QandA[currentQuestion].CorrectAnswer == i )
             {
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
+                
             }
             else
             {
@@ -63,12 +72,13 @@ public class QuizManager : MonoBehaviour
     }
 
 
+
     void GneraleQuestion()
     {
         if (QandA.Count > 0)
         {
             //質問をランダムで表示
-            //currentQuestion = Random.Range(0, QandA.Count);
+            currentQuestion = Random.Range(0, QandA.Count);
 
             //現在の質問がにゅうりょくされる
             Qtxt.text = QandA[currentQuestion].Question;
